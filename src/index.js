62e8aa2860291b079164292bd01907e7add042ec
Teodor Dacu <odo@192-168-0-159.rdsnet.ro>
2022-08-04 18:52:48 +0300

    chore: added dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>
-
+ReactDOM.render(
+  <h1>
     <img alt="" src="images/af1af9322d3a27ae9ea9a0a6a142b1c6f34217d9.png" />
-Hello, world</h1>, document.getElementById('root'));
+    Hello, world
+  </h1>,
+  document.getElementById('root')
+);
