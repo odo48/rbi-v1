042ae761ccc6abf63d376dc219c903e06dcbeb5d
Teodor Dacu <odo@192-168-0-159.rdsnet.ro>
2022-08-04 18:35:40 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>
+
+    <img alt="" src="images/af1af9322d3a27ae9ea9a0a6a142b1c6f34217d9.png" />
+Hello, world</h1>, document.getElementById('root'));
