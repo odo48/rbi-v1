7dc92b14ea86980b96c730fa98b50f70e52bae97
Teodor Dacu <odo@192-168-0-159.rdsnet.ro>
2022-08-09 17:38:50 +0300

    feat: started to work on history

---
+++ src/components/History/History.tsx
@@ -0,0 +1,46 @@
+import React, { useContext } from 'react';
+import styles from './History.module.scss';
+import { GeneralContext } from 'contexts/GeneralContext';
+import IGeneralContext from 'contexts/types';
+
+const History = () => {
+  const {
+    history,
+    setSelectedSection,
+    selectedSection,
+    position,
+    setPosition,
+  } = useContext(GeneralContext) as IGeneralContext;
+
+  const onClick = (increase: boolean) => {
+    setPosition(increase ? position + 1 : position - 1);
+
+    history &&
+      setSelectedSection(history[increase ? position + 1 : position - 1]);
+  };
+
+  console.log(history, position, selectedSection);
+
+  if (history && history.length <= 1) return <></>;
+
+  return (
+    <div className={styles['history']}>
+      <button
+        onClick={() => onClick(false)}
+        disabled={position === 0}
+        className={styles['history--btn']}
+      >
+        <i className="bi-chevron-left" />
+      </button>
+      <button
+        onClick={() => onClick(true)}
+        disabled={position + 1 === history?.length}
+        className={styles['history--btn']}
+      >
+        <i className="bi-chevron-right" />
+      </button>
+    </div>
+  );
+};
+
+export default History;
